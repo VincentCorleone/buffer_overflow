391d8182e8a4e4a2cba1dc70b54691ea6655eb5d
Vincent Chen <707119577@qq.com>
2016-03-22 21:21:37 +0800

    fisrt commit

---
+++ buffer_overflow.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+
+void overflow(const char* input)
+{
+       char buf[8];
+       printf("Virtual address of 'buf' = Ox%p\n", buf);
+       strcpy(buf,input);
+}
+
+void fun()
+{
+    printf("Function 'fun' has been called without an explicitly invocation.\n");
+    printf("Buffer Overflow attack succeeded!\n");
+	goto l1;
+}
+
+int main(int argc, char* argv[])
+{
+        printf("Virtual address of 'overflow' = Ox%p\n",overflow);
+        printf("Virtual address of 'fun' = Ox%p\n",fun);
+
+        char input[30]="01234567890123456789";
+		char add[5];
+		*((int*)add) = fun;
+		add[4] = 0;
+		strcat(input, add);
+        overflow(input);
+
+		l1:printf("The folling instructions in function main are excuted\nThe program ends");
+        return 0;
+}
